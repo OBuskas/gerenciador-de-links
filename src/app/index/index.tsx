ad3e6c4497f6f7e226b900feeafe448da9465908
Lucas Barbosa de Carvalho <lucas.barbosa12397@gmail.com>
2025-07-16 00:20:05 -0300

    Feat (index.tsx) Feature the first style sheet of the index

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -2,15 +2,13 @@ import {
   View,
   Text,
  } from 'react-native';
+import { styles } from './styles';
 
 export default function Index() {
   return (
-    <View>
-      <Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>
         Hello React Native!
-      </Text>
-      <Text>
-        Meu App
       </Text>
     </View>
   )
