0b1c4da29db21a87710a9abc031ff740e9f7f34e
Lucas Barbosa de Carvalho <lucas.barbosa12397@gmail.com>
2025-07-15 21:40:11 -0300

    Feat (index.tsx) Feature a Hello Word

---
+++ src/app/index.tsx
@@ -0,0 +1,17 @@
+import { 
+  View,
+  Text,
+ } from 'react-native';
+
+export default function Index() {
+  return (
+    <View>
+      <Text>
+        Hello React Native!
+      </Text>
+      <Text>
+        Meu App
+      </Text>
+    </View>
+  )
+}
