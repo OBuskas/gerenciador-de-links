625454e1df732674f7e455b5a447e4dc869fa13d
Lucas Barbosa de Carvalho <lucas.barbosa12397@gmail.com>
2025-07-16 19:54:45 -0300

    Feat (index.tsx, _layout.tsx, styles.ts, logo.png and tsconfig.json) Feature style index header buttons

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,15 +1,25 @@
 import { 
   View,
-  Text,
+  Image,
+  TouchableOpacity,
  } from 'react-native';
 import { styles } from './styles';
+import { MaterialIcons } from '@expo/vector-icons';
+import { colors } from '@/styles/colors';
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>
-        Hello React Native!
-      </Text>
+      <View style={styles.header}>
+        <Image 
+          source={require("@/assets/logo.png")} 
+          style={styles.logo}
+        />
+
+        <TouchableOpacity activeOpacity={0.7}>
+          <MaterialIcons name='add' size={32} color={colors.green[300]} />
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
