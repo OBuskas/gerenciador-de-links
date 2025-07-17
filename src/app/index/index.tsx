7f8c23f5e096d42ed662f764a87564901d9db3c4
Lucas Barbosa de Carvalho <lucas.barbosa12397@gmail.com>
2025-07-17 00:16:32 -0300

    Feat (category index.tsx, category styles.ts and app index.tsx) Feature category component

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -6,20 +6,19 @@ import {
 import { styles } from './styles';
 import { MaterialIcons } from '@expo/vector-icons';
 import { colors } from '@/styles/colors';
+import { Category } from "@/components/category";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Image 
-          source={require("@/assets/logo.png")} 
-          style={styles.logo}
-        />
-
+        <Image source={require("@/assets/logo.png")} style={styles.logo} />
         <TouchableOpacity activeOpacity={0.7}>
           <MaterialIcons name='add' size={32} color={colors.green[300]} />
         </TouchableOpacity>
       </View>
+
+      <Category />
     </View>
   )
 }
